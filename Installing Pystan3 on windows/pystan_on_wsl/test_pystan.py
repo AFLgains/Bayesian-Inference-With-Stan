7b4d15b855dce63d895e2ea4b4339a0599c343c3
aflgains <41205091+AFLgains@users.noreply.github.com>
2022-10-24 13:58:46 +1100

    update

--- Installing Pystan3 on windows/pystan_on_wsl/test_pystan.py
+++ Installing Pystan3 on windows/pystan_on_wsl/test_pystan.py
@@ -1,30 +1,15 @@
 import stan
-schools_code = """
-data {
-  int<lower=0> J;         // number of schools
-  real y[J];              // estimated treatment effects
-  real<lower=0> sigma[J]; // standard error of effect estimates
-}
+
+test_code = """
 parameters {
-  real mu;                // population treatment effect
-  real<lower=0> tau;      // standard deviation in treatment effects
-  vector[J] eta;          // unscaled deviation from mu by school
-}
-transformed parameters {
-  vector[J] theta = mu + tau * eta;        // school treatment effects
-}
+  real y; 
 model {
-  target += normal_lpdf(eta | 0, 1);       // prior log-density
-  target += normal_lpdf(y | theta, sigma); // log-likelihood
+  y~normal(0,1);
 }
 """
 
-schools_data = {"J": 8,
-                "y": [28,  8, -3,  7, -1,  1, 18, 12],
-                "sigma": [15, 10, 16, 11,  9, 11, 10, 18]}
-
-posterior = stan.build(schools_code, data=schools_data, random_seed=1)
+posterior = stan.build(test_code, random_seed=1)
 fit = posterior.sample(num_chains=4, num_samples=1000)
 print(fit)
-eta = fit["eta"]  # array with shape (8, 4000)
+eta = fit["y"] 
 print(eta)
